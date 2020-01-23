d5a62b9c57a32a351f1c15b4c28d322d9972ccfb
TheSynt4x <anonymousehaxor@gmail.com>
2020-01-23 22:22:25 +0100

    Changed directory name

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
 from flask import current_app as app
-from app.handlers.home import HomeController
+from app.controllers.home import HomeController
 
 app.add_url_rule('/', view_func=HomeController.as_view('home'))
