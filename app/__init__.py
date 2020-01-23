1f020b2f903c909bc4351cc92ab370a9c19ddbf1
TheSynt4x <anonymousehaxor@gmail.com>
2020-01-23 23:37:34 +0100

    Added some blog elements

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,13 @@ from flask import Flask
 def create_app():
   app = Flask(__name__, instance_relative_config=False, template_folder='../resources/views', static_folder='../public/')
 
+  app.config['APP_TITLE'] = 'Project'
+
   with app.app_context():
     from app import routes
+
+    @app.context_processor
+    def title():
+      return dict(title=app.config['APP_TITLE'])
 
     return app
