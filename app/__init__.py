a5c84b377d504486111bea4a1ad669304fde61c2
TheSynt4x <anonymousehaxor@gmail.com>
2020-01-24 22:05:02 +0100

    Added flask-dotenv library for autoloading env variables from file

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,24 @@
 from flask import Flask
+from flask_dotenv import DotEnv
+
+env = DotEnv()
 
 
 def create_app():
-  app = Flask(__name__, instance_relative_config=False, template_folder='../resources/views', static_folder='../public/')
+  app = Flask(
+    __name__,
+    instance_relative_config=False,
+    template_folder='../resources/views',
+    static_folder='../public/'
+  )
 
-  app.config['APP_TITLE'] = 'Project'
+  env.init_app(app, env_file='.env', verbose_mode=True)
 
   with app.app_context():
     from app import routes
 
     @app.context_processor
-    def title():
+    def site_globals():
       return dict(title=app.config['APP_TITLE'])
 
     return app
