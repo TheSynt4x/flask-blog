1f43b828f05ccf40cba2ec694ee3a285f9cb4cdf
TheSynt4x <anonymousehaxor@gmail.com>
2020-01-26 02:39:28 +0100

    Humanize datetime function added

--- app/utils/jinja.py
+++ app/utils/jinja.py
@@ -3,6 +3,7 @@ from flask import current_app as app
 from app.models.category import Category
 from app.models.post import Post
 
+from datetime import datetime
 
 @app.context_processor
 def site_globals():
@@ -18,3 +19,43 @@ def utility_processor():
     return Category.limit(5)
 
   return dict(recent_posts=recent_posts, categories=categories)
+
+
+@app.template_filter('humanize')
+def humanize_time(time):
+  """
+    Get a datetime object or a int() Epoch timestamp and return a
+    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+    'just now', etc
+    """
+  now = datetime.now()
+  diff = now - time
+  second_diff = diff.seconds
+  day_diff = diff.days
+
+  if day_diff < 0:
+    return ''
+
+  if day_diff == 0:
+    if second_diff < 10:
+      return "just now"
+    if second_diff < 60:
+      return str(int(second_diff)) + " seconds ago"
+    if second_diff < 120:
+      return "a minute ago"
+    if second_diff < 3600:
+      return str(int(second_diff / 60)) + " minutes ago"
+    if second_diff < 7200:
+      return "an hour ago"
+    if second_diff < 86400:
+      return str(int(second_diff / 3600)) + " hours ago"
+  if day_diff == 1:
+    return "yesterday"
+  if day_diff < 7:
+    return str(day_diff) + " days ago"
+  if day_diff < 31:
+    return str(int(day_diff / 7)) + " weeks ago"
+  if day_diff < 365:
+    return str(int(day_diff / 30)) + " months ago"
+  return str(int(day_diff / 365)) + " years ago"
+
