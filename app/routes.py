d355bb20f4a79fd558d216e8680d1b4e8ac64f91
TheSynt4x <anonymousehaxor@gmail.com>
2020-01-26 02:41:23 +0100

    Added login, register and search endpoints

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,16 @@
 from flask import current_app as app
 from app.controllers.home import HomeController
 from app.controllers.blog.post import PostController
+from app.controllers.blog.search import SearchController
 from app.controllers.blog.category import CategoryController
+from app.controllers.auth.login import LoginController
+from app.controllers.auth.register import RegisterController
 
 app.add_url_rule('/', view_func=HomeController.as_view('home'))
+
+app.add_url_rule('/search', view_func=SearchController.as_view('blog.search'))
 app.add_url_rule('/blog/post-<int:post_id>', view_func=PostController.as_view('blog.post'))
 app.add_url_rule('/category/<int:category_id>', view_func=CategoryController.as_view('blog.category'))
+
+app.add_url_rule('/login', view_func=LoginController.as_view('login'))
+app.add_url_rule('/register', view_func=RegisterController.as_view('register'))
