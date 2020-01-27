44f9d29c6e2c93d55bfab84879d0bca4634273d3
TheSynt4x <anonymousehaxor@gmail.com>
2020-01-27 23:43:15 +0100

    Documentation added

--- app/controllers/auth/login.py
+++ app/controllers/auth/login.py
@@ -8,10 +8,22 @@ from app.models.user import User
 class LoginController(MethodView):
   @auth.optional
   def get(self):
+    """
+    Renders the login template
+
+    Returns:
+      The login template
+    """
     return render_template('auth/login.html')
 
   @auth.optional
   def post(self):
+    """
+    Processes the POST request and checks if credentials match against the database
+
+    Returns:
+      A redirect
+    """
     username = request.form.get('username')
     password = request.form.get('password')
 
