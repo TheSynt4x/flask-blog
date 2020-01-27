365561568b2fab2748f90d78baa2c0c0c3df9d30
TheSynt4x <anonymousehaxor@gmail.com>
2020-01-27 23:44:37 +0100

    Abstracted code to avatar service

--- app/controllers/auth/register.py
+++ app/controllers/auth/register.py
@@ -1,13 +1,10 @@
-import os
-import uuid
-
-from flask import render_template, flash, redirect, url_for, current_app as app, request
+from flask import render_template, flash, redirect, url_for, request
 from flask.views import MethodView
-from werkzeug import secure_filename
 
 from app.middleware import auth
 from app.models.user import User
 from app.validators.register_form import RegisterForm
+from app.services import avatar_service
 
 
 class RegisterController(MethodView):
@@ -22,19 +19,10 @@ class RegisterController(MethodView):
     if form.validate_on_submit():
       form.validate_username(form.username)
 
-      unique_filename = ''
+      avatar = 'no-image.png'
 
       if 'avatar' in request.files and request.files['avatar']:
-        file = secure_filename(form.avatar.data.filename)
-        extension = os.path.splitext(file)[1]
-
-        unique_filename = str(uuid.uuid4()) + extension
-
-        form.avatar.data.save(os.path.join(
-          app.static_folder, 'images', unique_filename
-        ))
-
-      avatar = unique_filename if 'avatar' in request.files and request.files['avatar'] else 'no-image.png'
+        avatar = avatar_service.save(form.avatar.data)
 
       User.create(form.username.data, form.password.data, avatar)
 
