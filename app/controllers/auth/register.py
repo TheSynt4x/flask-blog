f8f965471171a54dece2893a638f84a414caaa75
TheSynt4x <anonymousehaxor@gmail.com>
2020-02-09 22:36:48 +0100

    Documentation added

--- app/controllers/auth/register.py
+++ app/controllers/auth/register.py
@@ -10,10 +10,22 @@ from app.services import avatar_service
 class RegisterController(MethodView):
   @auth.optional
   def get(self):
+    """
+    Show register form
+
+    Returns:
+      Register template with form
+    """
     return render_template('auth/register.html', form=RegisterForm())
 
   @auth.optional
   def post(self):
+    """
+    Handle the POST request and sign up the user if form validation passes
+
+    Returns:
+      A redirect or a template with the validation errors
+    """
     form = RegisterForm()
 
     if form.validate_on_submit():
